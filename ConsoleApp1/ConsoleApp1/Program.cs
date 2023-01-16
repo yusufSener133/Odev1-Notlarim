1c8c92d2c75f3c21905bd135216c7931038daf33
Yusuf Sener <yusuf_sener_97@hotmail.com>
2023-01-16 19:10:49 +0300

    Added C# project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 19; i++)
+            {
+            Console.WriteLine("Hello World!\t"+(i+1));
+
+            }
+        }
+    }
+}
